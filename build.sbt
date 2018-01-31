f8175392528bf0dad5e9726cb5da4796c5aeca45
广陵大琴渣 <jack.sha@zmlearn.com>
2018-01-31 13:20:25 +0800

    save

--- build.sbt
+++ build.sbt
@@ -3,6 +3,8 @@ enablePlugins(ScalaJSPlugin)
 name := "Scala.js Tutorial"
 scalaVersion := "2.12.2" // or any other Scala version >= 2.10.2
 
+// scalacOptions += "-Xxml:coalescing" //support cdata style
+
 // This is an application with a main method
 scalaJSUseMainModuleInitializer := true
 
--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,24 +1,51 @@
 package tutorial.webapp
-import com.thoughtworks.binding.Binding
+// import com.thoughtworks.binding.Binding
+// import com.thoughtworks.binding.dom
+// import scala.scalajs.js.timers.setInterval
+// import java.util.Date
+
+// import com.thoughtworks.binding.Binding._
+
+// import com.thoughtworks.binding.Binding.Var
+// import com.thoughtworks.binding.Binding.Vars
+// import org.scalajs.dom.document
+// import org.scalajs.dom.raw.Event
+// import org.scalajs.dom._
 import com.thoughtworks.binding.dom
-
-import com.thoughtworks.binding.Binding._
-
+import java.util.Date
 import com.thoughtworks.binding.Binding.Var
 import com.thoughtworks.binding.Binding.Vars
-import com.thoughtworks.binding.Binding.BindingSeq
+import com.thoughtworks.binding.Binding
+import scala.scalajs.js.timers.setInterval
+import org.scalajs.dom.raw.HTMLButtonElement
 import org.scalajs.dom.document
 import org.scalajs.dom.raw.Event
+
+
+import org.scalajs.dom.raw.HTMLButtonElement
+
+
+
 
 object TutorialApp {
   
   case class Contact(name: Var[String], email: Var[String])
 
   val data = Vars.empty[Contact]
-
-  @dom
-  def table = {
+  val now = Var(new Date)
+  setInterval(1000) { now := new Date }
+  @dom def randomParagraph = {
+    <p>生成一个随机数： { math.random.toString }</p>
+  }
+  @dom def comment = {
+    <!-- 你看不见我 -->
+  }
+   
+  @dom def table = {
     <div>
+     现在时间：{ now.bind.toString } 哈哈
+     { randomParagraph.bind }
+      { comment.bind }
       <button
         onclick={ event: Event =>
           data.get += Contact(Var("Yang Bo"), Var("yang.bo@rea-group.com"))
